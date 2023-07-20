0a40167077f6ebffaf7e718851c9c2b9ccab1884
Mariette Olwagen <90972322+MarietteO@users.noreply.github.com>
2023-07-21 00:10:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,43 @@
 game_on = True
 
 def direction():
-    direction_choice = input("Which way would you like to go? Type left or right: ").lower()
-    if direction_choice == "right":
-        game_over("Oh no! You chose the wrong path and fell into a pit of quicksand.")
-        return False
-    elif direction_choice == "left":
-        return True
-    else:
-        not_valid_answer()
-        direction()
+    while True:
+        direction_choice = input("Which way would you like to go? Type left or right: ").lower()
+        if direction_choice == "right":
+            game_over("Oh no! You chose the wrong path and fell into a pit of quicksand.")
+            return False
+        elif direction_choice == "left":
+            return True
+        else:
+            not_valid_answer()
 
 def action():
-    action_choice = input("You reach a river. Do you want to swim or wait? Type swim or wait: ").lower()
-    if action_choice == "swim":
-        game_over("You are attacked by a fierce sea monster while swimming across the river.")
-        return False
-    elif action_choice == "wait":
-        return True
-    else:
-        not_valid_answer()
-        action()
+    while True:
+        action_choice = input("You reach a river. Do you want to swim or wait? Type swim or wait: ").lower()
+        if action_choice == "swim":
+            game_over("You are attacked by a fierce sea monster while swimming across the river.")
+            return False
+        elif action_choice == "wait":
+            return True
+        else:
+            not_valid_answer()
 
 def choose_door():
-    door_choice = input("While waiting, you spot three doors. Which door do you choose? Type red, blue,"
-                        " or yellow: ").lower()
-    if door_choice == "red":
-        game_over("Oops! As you step through the door, you trigger a trap and get caught.")
-        return False
-    elif door_choice == "blue":
-        game_over("The blue door leads to a room filled with treasure, but it's protected by a fire-breathing dragon."
-                  " You are toast!")
-        return False
-    elif door_choice == "yellow":
-        print("Congratulations! You've chosen the door of wisdom and found the treasure! You win!")
-        return True
-    else:
-        not_valid_answer()
-        choose_door()
+    while True:
+        door_choice = input("While waiting, you spot three doors. Which door do you choose? Type red, blue,"
+                            " or yellow: ").lower()
+        if door_choice == "red":
+            game_over("Oops! As you step through the door, you trigger a trap and get caught.")
+            return False
+        elif door_choice == "blue":
+            game_over("The blue door leads to a room filled with treasure, but it's protected by a fire-breathing dragon."
+                      " You are toast!")
+            return False
+        elif door_choice == "yellow":
+            print("Congratulations! You've chosen the door of wisdom and found the treasure! You win!")
+            return True
+        else:
+            not_valid_answer()
 
 def not_valid_answer():
     print("Argh! You're making me dizzy! Please choose a valid answer.")
@@ -80,3 +80,4 @@ while game_on:
         break
     else:
         break
+
