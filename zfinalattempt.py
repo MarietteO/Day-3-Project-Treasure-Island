3382f667578ba4b085bc655e6abbf234370b71d1
Mariette Olwagen <90972322+MarietteO@users.noreply.github.com>
2024-11-20 21:55:06 +0200

    Update zfinalattempt.py

--- zfinalattempt.py
+++ zfinalattempt.py
@@ -16,11 +16,11 @@ outcomes = {
     "yellow": "Congratulations! You've chosen the door of wisdom and found the treasure! You win!",
 }
 
-def ask(answer, answers):
+def ask(ques, answers):
     """Function to ask the user a question and get their answer. Also loops back to current question if invalid
     answer received."""
-    while True:
-        user_answer = input(answer)
+    while True: #ensures that the program keeps asking the user the same question until a valid answer is provided
+        user_answer = input(ques)
         if user_answer in answers:
             return user_answer
         else:
@@ -29,8 +29,8 @@ def ask(answer, answers):
 def game():
     """Function to play the adventure game based on user choices."""
     for question in steps:
-        possible_answers = steps[question]
-        user_response = ask(question, possible_answers)
+        possible_answers = steps[question] #retrieves the values for each key in the "steps" dictionary. For each key, saves them in a variable called "possible answers"
+        user_response = ask(question, possible_answers) #inputs each value key pair into the function called ask
         if user_response in outcomes:
             print(outcomes[user_response])
             break
